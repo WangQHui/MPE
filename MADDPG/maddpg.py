623c935fafd652ba98bdfb6a15a2cef069ad2202
WangQHui <1726422946@qq.com>
2021-05-05 22:03:48 +0800

    create MADDPG

---
+++ MADDPG/maddpg.py
@@ -0,0 +1,212 @@
+import random
+import datetime
+import numpy as np
+from collections import deque
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Lambda, Concatenate
+from tensorflow.keras.optimizers import Adam
+import time
+from MADDPG.replay_buffer import ReplayBuffer
+
+tf.keras.backend.set_floatx('float32')
+gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+# Actor Model
+def actor(state_shape, action_dim):
+    state = Input(shape=state_shape)
+    x = Dense(64, activation='relu')(state)
+    x = Dense(32, activation='relu')(x)
+    output = Dense(action_dim, activation='tanh')(x)  # 在网络末端，用tanh函数，把输出映射到[-1.0,1.0]之间
+    model = Model(inputs=state, outputs=output)
+    return model
+
+
+# Critic Model
+def critic(state_shapes, action_dims, agent_num):
+    inputs = []
+    for a in range(agent_num):
+        inputs.append(Input(shape=state_shapes[a]))
+    for a in range(agent_num):
+        inputs.append(Input(shape=action_dims[a]))
+    concat = Concatenate(axis=-1)(inputs)
+    x = Dense(128, activation='relu')(concat)
+    x = Dense(64, activation='relu')(x)
+    output = Dense(1)(x)
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+# update network parameters
+def update_target_weights(model, target_model, tau):
+    weights = model.get_weights()
+    target_weights = target_model.get_weights()
+    for i in range(len(target_weights)):  # set tau% of target model to be new weights
+        target_weights[i] = weights[i] * tau + target_weights[i] * (1 - tau)
+    target_model.set_weights(target_weights)
+
+
+# MADDPGAgent Class
+class MADDPGAgent():
+    def __init__(self, name, obs_shape, act_space, agent_num, agent_index, parameters, create_summary_writer=False):
+        self.name = name
+        self.obs_shape = obs_shape
+        self.act_space = act_space
+        self.agent_num = agent_num
+        self.agent_index = agent_index
+        self.parameters = parameters
+
+        self.actor = actor(state_shape=obs_shape[self.agent_index].shape[0], action_dim=act_space[self.agent_index].shape[0])
+        self.critic = critic(state_shapes=[obs.shape[0]for obs in obs_shape], action_dims=[act.shape[0] for act in act_space], agent_num=agent_num)
+
+        self.actor_target = actor(state_shape=obs_shape[self.agent_index].shape[0], action_dim=act_space[self.agent_index].shape[0])
+        self.critic_target = critic(state_shapes=[obs.shape[0]for obs in obs_shape], action_dims = [act.shape[0] for act in act_space], agent_num = agent_num)
+
+        self.actor_optimizer = Adam(learning_rate=parameters['lr_actor'])
+        self.critic_optimizer = Adam(learning_rate=parameters['lr_critic'])
+        # self.critic.compile(loss="mean_squared_error", optimizer=self.critic_optimizer)
+
+        self.action_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(act_space[self.agent_index].shape[0]), sigma=parameters['sigma'])
+
+        self.update_networks_weight(tau=1)
+
+        # Create experience buffer
+        self.replay_buffer = ReplayBuffer(parameters["buffer_size"])
+        self.max_replay_buffer_len = parameters['max_replay_buffer_len']
+        self.replay_sample_index = None
+
+        # 为每一个agent构建tensorboard可视化训练过程
+        if create_summary_writer:
+            current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            train_log_dir = 'logs/MADDPG_Summary_' + current_time + "agent" + str(self.agent_index)
+            self.summary_writer = tf.summary.create_file_writer(train_log_dir)
+
+    def update_networks_weight(self, tau=1):
+        update_target_weights(model=self.actor, target_model=self.actor_target, tau=tau)
+        update_target_weights(model=self.critic, target_model=self.critic_target, tau=tau)
+
+    def action(self, obs, evaluation=False):
+        obs = np.expand_dims(obs, axis=0).astype(np.float32)
+        mu, pi = self.__get_action(obs)
+        a = mu if evaluation else pi
+        return np.array(a[0])
+
+    @tf.function
+    def __get_action(self, obs):
+        mu = self.actor(obs)
+        noise = np.asarray([self.action_noise() for i in range(mu.shape[0])])
+        # print(noise)
+        pi = tf.clip_by_value(mu + noise, -1, 1)
+        return mu, pi
+
+    def experience(self, obs, act, rew, new_obs, done):
+        # Store transition in the replay buffer.
+        self.replay_buffer.add(obs, act, rew, new_obs, [float(done[0])])
+
+    # save_model("models/maddpg_actor_agent_", "models/maddpg_critic_agent_")
+    def save_model(self, a_fn, c_fn):
+        self.actor.save(a_fn+str(self.agent_index)+".h5")
+        self.critic.save(c_fn+str(self.agent_index)+".h5")
+
+    def load_actor(self, a_fn):
+        self.actor.load_weights(a_fn+str(self.agent_index)+".h5")
+
+    def preupdate(self):
+        self.replay_sample_index = None
+
+    def update(self, agents, train_step):
+        T = time.time()
+        self.replay_sample_index = self.replay_buffer.make_index(self.parameters['batch-size'])
+        # collect replay sample from all agents
+        obs_n = []
+        act_n = []
+        obs_next_n = []
+        rew_n = []
+        done_n = []
+        act_next_n = []
+
+        for i in range(self.agent_num):
+            obs, act, rew, obs_next, done = agents[i].replay_buffer.sample_index(self.replay_sample_index)
+            obs_n.append(obs)
+            obs_next_n.append(obs_next)
+            act_n.append(act)
+            done_n.append(done)
+            rew_n.append(rew)
+
+        for i, agent in enumerate(agents):
+            target_mu = agents[i].actor_target(obs_next_n[i])
+            # action_target = tf.clip_by_value(target_mu + self.action_noise(), -1, 1)
+            act_next_n.append(target_mu)
+        # print(time.time()-T)
+        '''
+        obs_n = tf.convert_to_tensor(obs_n, dtype=tf.float32)
+        act_n = tf.convert_to_tensor(act_n, dtype=tf.float32)
+        rew_n = tf.convert_to_tensor(rew_n, dtype=tf.float32)
+        obs_next_n = tf.convert_to_tensor(obs_next_n, dtype=tf.float32)
+        done_n = tf.convert_to_tensor(done_n, dtype=tf.float32)
+        '''
+
+        summaries = self.train((obs_n, act_n, rew_n, obs_next_n, done_n, act_next_n))
+
+        # if train_step % 10 == 0:  # only update every 100 steps
+        self.update_networks_weight(tau=self.parameters["tau"])
+
+        with self.summary_writer.as_default():
+            for key in summaries.keys():
+                tf.summary.scalar(key, summaries[key], step=train_step)
+        self.summary_writer.flush()
+
+    @tf.function
+    def train(self, memories):
+        obs_n, act_n, rew_n, obs_next_n, done_n, act_next_n = memories
+        with tf.GradientTape() as tape:
+            # compute Q_target
+            q_target = self.critic_target(obs_next_n+act_next_n)
+            dc_r = rew_n[self.agent_index] + self.parameters['gamma'] * q_target * (1 - done_n[self.agent_index])
+            # compute Q
+            q = self.critic(obs_n + act_n)
+            td_error = q - dc_r
+            q_loss = tf.reduce_mean(tf.square(td_error))
+        q_grads = tape.gradient(q_loss, self.critic.trainable_variables)
+        self.critic_optimizer.apply_gradients(zip(q_grads, self.critic.trainable_variables))
+
+        with tf.GradientTape() as tape:
+            mu = self.actor(obs_n[self.agent_index])
+            act_n = [mu if i == self.agent_index else act for i, act in enumerate(act_n)]
+            # act_n[self.agent_index] = mu
+            q_actor = self.critic(obs_n + act_n)
+            actor_loss = -tf.reduce_mean(q_actor)
+        actor_grads = tape.gradient(actor_loss, self.actor.trainable_variables)
+        self.actor_optimizer.apply_gradients(zip(actor_grads, self.actor.trainable_variables))
+
+        summaries = dict([
+            ['LOSS/actor_loss', actor_loss],
+            ['LOSS/q_loss', q_loss],
+        ])
+
+        return summaries
+
+
+# Based on http://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
+class OrnsteinUhlenbeckActionNoise(object):
+    def __init__(self, mu, sigma=0.2, theta=.15, dt=1e-2, x0=None):
+        self.theta = theta
+        self.mu = mu
+        self.sigma = sigma
+        self.dt = dt
+        self.x0 = x0
+        self.reset()
+
+    def __call__(self):
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+        self.x_prev = x
+        return x
+
+    def reset(self):
+        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
+
+    def __repr__(self):
+        return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
---
+++ MADDPG/train.py
@@ -0,0 +1,166 @@
+import sys
+import os
+import argparse
+import numpy as np
+import tensorflow as tf
+import time
+import pickle
+import datetime
+from MADDPG.maddpg import MADDPGAgent
+from make_env import make_env
+
+train_parameters = {
+    "max-episode-len": 25,
+    "num-episodes": 1000,
+    }
+model_parameters = {
+    "buffer_size": 1000,
+    "lr_actor": 1.0e-2,
+    "lr_critic": 1.0e-2,
+    "sigma": 0.15,
+    "gamma": 0.95,
+    "batch-size": 1024,
+    "max_replay_buffer_len": 10240,
+    "tau": 0.01
+}
+
+
+def train():
+    # 初始化环境
+    env = make_env(scenario_name="Predator_prey_4v4")
+
+    # 初始化MADDPGAgent
+    maddpgagents = [MADDPGAgent(name="agent_"+str(i),
+                                obs_shape=env.observation_space,
+                                act_space=env.action_space,
+                                agent_num=env.n,
+                                agent_index=i,
+                                parameters=model_parameters,
+                                create_summary_writer=True)
+                    for i in range(env.n)]
+
+    print('Starting training...')
+    episode = 0
+    epoch = 0
+    train_step = 0
+    while episode < train_parameters["num-episodes"]:
+        t_start = time.time()
+        obs_n = env.reset()
+        episode_steps = 0
+        episode_rewards = [0.0]  # sum of rewards for all agents
+        group_rewards = [[0.0], [0.0]]
+        agent_rewards = [[0.0] for _ in range(env.n)]  # individual agent reward
+
+        for agent in maddpgagents:
+            agent.action_noise.reset()
+
+        while episode_steps < train_parameters["max-episode-len"]:
+            T = time.time()
+            # get action
+            action_n = [agent.action(obs, evaluation=False) for agent, obs in zip(maddpgagents, obs_n)]
+            # print(time.time()-T)
+            # environment step
+            # print(action_n)
+            new_obs_n, rew_n, done_n, _ = env.step(action_n)
+            done = all(done_n)
+            # collect experience
+            for i, agent in enumerate(maddpgagents):
+                agent.experience(obs_n[i], action_n[i], [rew_n[i]], new_obs_n[i], [done_n[i]])
+
+            # 记录reward数据
+            for i, rew in enumerate(rew_n):
+                episode_rewards[-1] += rew
+                agent_rewards[i][-1] += rew
+            group_rewards[0][-1] += sum(rew_n[:4])
+            group_rewards[1][-1] += sum(rew_n[4:])
+
+            # update all trainers
+            if epoch % 50 == 0:
+                for agent in maddpgagents:
+                    agent.preupdate()
+                train_able = False
+                for i in range(env.n):
+                    if len(maddpgagents[i].replay_buffer) > maddpgagents[i].max_replay_buffer_len:  # replay buffer is not large enough
+                        train_able = True
+                        maddpgagents[i].update(maddpgagents, train_step)
+                if train_able:
+                    train_step += 1
+
+            if done:
+                break
+
+            obs_n = new_obs_n
+            episode_steps += 1
+            epoch += 1
+
+            # print(time.time()-T)
+        if episode % 50 == 0:
+            print("episode {}: {} total reward, {} epoch, {} episode_steps, {} train_steps, {} time".format(
+                episode, agent_rewards, epoch, episode_steps, train_step, time.time()-t_start))
+
+        episode += 1
+
+        for i, maddpgagent in enumerate(maddpgagents):
+            with maddpgagent.summary_writer.as_default():
+                tf.summary.scalar('Main/total_reward', episode_rewards[-1], step=episode)
+                tf.summary.scalar('Main/Agent_reward', agent_rewards[i][-1], step=episode)
+                if i < 4:
+                    tf.summary.scalar('Main/group_reward', group_rewards[0][-1], step=episode)
+                else:
+                    tf.summary.scalar('Main/group_reward', group_rewards[1][-1], step=episode)
+            maddpgagent.summary_writer.flush()
+
+        if episode % 50 == 0:
+            # 保存模型参数
+            for agent in maddpgagents:
+                agent.save_model("models/maddpg_actor_agent_", "models/maddpg_critic_agent_")
+
+    # 关闭summary，回收资源
+    for i, maddpgagent in enumerate(maddpgagents):
+        maddpgagent.summary_writer.close()
+    env.close()
+    # 保存模型参数
+    for agent in maddpgagents:
+        agent.save_model("models/maddpg_actor_agent_", "models/maddpg_critic_agent_")
+
+
+def inference(episode_num=100, max_episode_steps=100):
+    # 初始化环境
+    env = make_env(scenario_name="Predator_prey_3v3")
+
+    # 初始化MADDPGAgent
+    maddpgagents = [MADDPGAgent(name="agent" + str(i),
+                                obs_shape=env.observation_space,
+                                act_space=env.action_space,
+                                agent_num=env.n,
+                                agent_index=i,
+                                parameters=model_parameters,
+                                create_summary_writer=False)
+                    for i in range(env.n)]
+    for agent in maddpgagents:
+        agent.load_actor(a_fn="models/maddpg_actor_agent_")
+
+    episode = 0
+    while episode < episode_num:
+        rewards = np.zeros(env.n, dtype=np.float32)
+        cur_state = env.reset()
+        step = 0
+        while step < max_episode_steps:
+            # get action
+            action_n = [agent.action(obs, evaluation=True) for agent, obs in zip(maddpgagents, cur_state)]
+            # print(action_n)
+            # environment step
+            next_state, reward, done, _ = env.step(action_n)
+            # env.render()
+            time.sleep(0.03)
+            cur_state = next_state
+            rewards += np.asarray(reward, dtype=np.float32)
+            step += 1
+        episode += 1
+        print("episode {}: {} total reward, {} steps".format(episode, rewards, step))
+    # env.close()
+
+
+if __name__ == '__main__':
+    # train()
+    inference()
